d11b2094a439489d89a494b03076900901df8d15
Ali Ahmed <ali.ali.ameen245@gmail.com>
2023-01-08 00:28:51 +0200

    added succes modal for crowdfunding page

--- Crowdfunding-Product-Page-Main/app.js
+++ Crowdfunding-Product-Page-Main/app.js
@@ -1,6 +1,8 @@
+/* Mobile menu for navigation */
 const menuBtn = document.querySelector('.menu-btn')
 const nav = document.querySelector('.nav')
 let active = false
+
 menuBtn.addEventListener('click', event => {
     if (!active) {
         menuBtn.classList.add('active')
@@ -12,8 +14,11 @@ menuBtn.addEventListener('click', event => {
         active = false
     }
 })
+
+/* Bookmark project */
 const bookmarkBtn = document.querySelector('.product-header__bookmark-button')
 let bookmarked = false
+
 bookmarkBtn.addEventListener('click', event => {
     if (!bookmarked) {
         bookmarkBtn.classList.add('bookmarked')
@@ -24,11 +29,19 @@ bookmarkBtn.addEventListener('click', event => {
     }
 })
 
+/* Show modal for back project */
 const backBtn = document.querySelector('.product-header__back-project-button')
 const closeBtn = document.querySelector('.main-model__close-modal')
 const backProject = document.querySelector('.back-project-model')
 let showBackProject = false
-backBtn.addEventListener('click', event => {
+let selected = null
+
+
+function toggleModal() {
+    if (selected) {
+        selected.classList.remove('active')
+        selected = null
+    }
     if (!showBackProject) {
         backProject.classList.add('show')
         document.body.style.overflow = 'hidden'
@@ -38,15 +51,48 @@ backBtn.addEventListener('click', event => {
         document.body.style.overflow = 'auto'
         showBackProject = false
     }
+}
+
+backBtn.addEventListener('click', toggleModal)
+closeBtn.addEventListener('click', toggleModal)
+
+/* Select project and pledge */
+const available = document.querySelectorAll('.available')
+available.forEach(element => {
+    element.addEventListener('click', event => {
+        if (!selected) {
+            selected = element
+            selected.classList.add('active')
+        } else {
+            selected.classList.remove('active')
+            selected = element
+            selected.classList.add('active')
+        }
+    })
 })
-closeBtn.addEventListener('click', event => {
-    if (!showBackProject) {
-        backProject.classList.add('show')
-        document.body.style.overflow = 'hidden'
-        showBackProject = true
-    } else {
-        backProject.classList.remove('show')
+
+/* Success modal after click on continue button */
+const continueButtons = document.querySelectorAll('.main-model__selection-option-pledge-continue')
+const successModal = document.querySelector('.success-modal-container')
+const successModalButton = document.querySelector('.success-modal__button')
+let showSuccessModal = false
+
+continueButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        if (!showSuccessModal) {
+            successModal.classList.add('active')
+            backProject.classList.remove('show')
+            document.body.style.overflow = 'hidden'
+            showBackProject = false
+            showSuccessModal = true
+        }
+    })
+})
+
+successModalButton.addEventListener('click', event => {
+    if (showSuccessModal) {
+        successModal.classList.remove('active')
         document.body.style.overflow = 'auto'
-        showBackProject = false
+        showSuccessModal = false
     }
 })
